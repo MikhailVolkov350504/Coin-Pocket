9a91062faf3537a5bc1d6b30ad7b94b4f923d337
Mihail Volkov 350504 <ivolkov95@gmail.com>
2016-03-01 16:24:17 +0300

    Countries list added

--- src/sample/controller/CollectionBoardController.java
+++ src/sample/controller/CollectionBoardController.java
@@ -1,8 +1,10 @@
 package sample.controller;
 
-import javafx.event.Event;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import sample.model.ServerManager;
 import sample.model.network.callback.CoinSetsCallback;
 import sample.model.network.callback.CoinsCallback;
@@ -24,76 +26,151 @@ public class CollectionBoardController implements
         CoinSetsCallback,
         CoinsCallback {
 
-    public ListView continentListView;
-    public ListView countryListView;
-    public ListView coinSetListView;
-    public ListView coinListView;
+//    public ListView continentListView;
+//    public ListView countryListView;
+//    public ListView coinSetListView;
+//    public ListView coinListView;
+    public ArrayList<String> countriesList;
+    public TreeItem<String> currentContinent;
+    public int continentsIndex = 0;
+
+    @FXML
+    private TreeView countryTree;
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        countriesList = new ArrayList<String>();
         ServerManager.getContinents(this);
+        setContinents();
+
     }
 
     //Actions
-    public void continentViewTouched(Event event) {
-
-        countryListView.getItems().clear();
-        coinSetListView.getItems().clear();
-        coinListView.getItems().clear();
-
-        String continentName = (String)continentListView.getSelectionModel().getSelectedItem();
-        ServerManager.getCountries(continentName, this);
-    }
-
-    public void countryViewTouched(Event event) {
-
-        coinSetListView.getItems().clear();
-        coinListView.getItems().clear();
-
-        String countryName = (String)countryListView.getSelectionModel().getSelectedItem();
-        ServerManager.getCointSets(countryName, this);
-    }
-
-    public void coinSetViewTouched(Event event) {
-
-        coinListView.getItems().clear();
-
-        String countryName = (String)countryListView.getSelectionModel().getSelectedItem();
-        String years = (String)coinSetListView.getSelectionModel().getSelectedItem();
-        ServerManager.getCoins(countryName, years, this);
-    }
-
-    public void coinViewTouched(Event event) {
-        System.out.println(coinListView.getSelectionModel().getSelectedItem());
-    }
+//    public void continentViewTouched(Event event) {
+//
+//        countryListView.getItems().clear();
+//        coinSetListView.getItems().clear();
+//        coinListView.getItems().clear();
+//
+//        String continentName = (String)continentListView.getSelectionModel().getSelectedItem();
+//        ServerManager.getCountries(continentName, this);
+//    }
+//
+//    public void countryViewTouched(Event event) {
+//
+//        coinSetListView.getItems().clear();
+//        coinListView.getItems().clear();
+//
+//        String countryName = (String)countryListView.getSelectionModel().getSelectedItem();
+//        ServerManager.getCointSets(countryName, this);
+//    }
+//
+//    public void coinSetViewTouched(Event event) {
+//
+//        coinListView.getItems().clear();
+//
+//        String countryName = (String)countryListView.getSelectionModel().getSelectedItem();
+//        String years = (String)coinSetListView.getSelectionModel().getSelectedItem();
+//        ServerManager.getCoins(countryName, years, this);
+//    }
+//
+//    public void coinViewTouched(Event event) {
+//        System.out.println(coinListView.getSelectionModel().getSelectedItem());
+//    }
 
     //Callbacks
     @Override
     public void continentsReceived(ArrayList<Continent> continents) {
-        for (Continent continent:continents)
-            continentListView.getItems().addAll(continent.getName());
+        for (Continent continent:continents) {
+//            continentListView.getItems().addAll(continent.getName());
+//            addContinents(continent.getName(),countryTree.getRoot());
+            System.out.println(continent.getName());
+        }
+
     }
 
     @Override
     public void countriesReceived(ArrayList<Country> countries) {
-        for (Country country:countries)
-            countryListView.getItems().addAll(country.getName());
+        for (Country country:countries) {
+            countriesList.add(country.getName());
+        }
+//        System.out.println(countriesList);
+        addCountries();
     }
 
     @Override
     public void coinSetsReceived(ArrayList<CoinSet> sets) {
-        for (CoinSet set:sets)
-            coinSetListView.getItems().addAll(set.getYears());
+//        for (CoinSet set:sets)
+//            coinSetListView.getItems().addAll(set.getYears());
     }
 
     @Override
     public void coinsReceived(ArrayList<Coin> coins) {
-        for (Coin coin:coins)
-            coinListView.getItems().addAll(coin.getNominal() + " " + coin.getCurrency());
+//        for (Coin coin:coins)
+//            coinListView.getItems().addAll(coin.getNominal() + " " + coin.getCurrency());
     }
 
     @Override
     public void errorReceived(String errorMessage) {
         System.out.println(errorMessage);
     }
+
+    public void setContinents(){
+        //create root
+        TreeItem<String> root = new TreeItem<>("Continents");
+        root.setExpanded(true);
+        TreeItem<String> asia = new TreeItem<>("Asia");
+        TreeItem<String> africa = new TreeItem<>("Africa");
+        TreeItem<String> australia = new TreeItem<>("Australia");
+        TreeItem<String> europe = new TreeItem<>("Europe");
+        TreeItem<String> latAmerica = new TreeItem<>("L.America");
+        TreeItem<String> northAmerica = new TreeItem<>("N.America");
+        root.getChildren().addAll(asia, africa, australia, europe, latAmerica, northAmerica);
+        countryTree.setRoot(root);
+        getCountriesFromServer(root.getChildren());
+    }
+
+
+
+
+    public void getCountriesFromServer(ObservableList<TreeItem<String>> continentsList){
+//        for(int i = 0; i < continentsList.size(); i++){
+            String continent = continentsList.get(continentsIndex).getValue();
+            switch (continent){
+                case "Asia": currentContinent = continentsList.get(continentsIndex);
+                    ServerManager.getCountries(continent, this);
+                    break;
+                case "Africa": currentContinent = continentsList.get(continentsIndex);
+                    ServerManager.getCountries(continent, this);
+                    break;
+                case "Europe": currentContinent = continentsList.get(continentsIndex);
+                    ServerManager.getCountries(continent, this);
+                    break;
+                case "L.America": currentContinent = continentsList.get(continentsIndex);
+                    ServerManager.getCountries(continent, this);
+                    break;
+                case "N.America": currentContinent = continentsList.get(continentsIndex);
+                    ServerManager.getCountries(continent, this);
+                    break;
+                case "Australia": currentContinent = continentsList.get(continentsIndex);
+                    ServerManager.getCountries(continent, this);
+            }
+//        }
+    }
+
+    public void addCountries(){
+        for(int i = 0; i < countriesList.size();i++){
+            TreeItem<String> treeItem = new TreeItem<>(countriesList.get(i));
+            currentContinent.getChildren().add(treeItem);
+        }
+        countriesList.clear();
+        continentsIndex = continentsIndex + 1;
+        if(continentsIndex < countryTree.getRoot().getChildren().size())
+        getCountriesFromServer(countryTree.getRoot().getChildren());
+
+    }
+
 }
+
