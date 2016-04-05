32290c46b05b2358394f2ec6e24a876ac36a9cbd
Pavel Evsaev 350504 <pavel.evsaev@gmail.com>
2016-04-05 20:45:05 +0300

    added activityIndicators. updated login screens layout

--- src/sample/controller/SignInController.java
+++ src/sample/controller/SignInController.java
@@ -8,7 +8,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Screen;
@@ -30,17 +32,24 @@ public class SignInController implements SignInCallback, Initializable {
     @FXML private Text infoMessageText;
     @FXML private Button signInButton;
     @FXML private Button signUpButton;
+    @FXML private ProgressIndicator activityIndicator;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        emailField.addEventFilter(KeyEvent.KEY_TYPED, event -> {
+            infoMessageText.setText("");
+        });
+
+        passwordField.addEventFilter(KeyEvent.KEY_TYPED, event -> {
+            infoMessageText.setText("");
+        });
         emailField.setText("volchik@gmail.com");
         passwordField.setText("12345678");
     }
 
     //Action handlers
     public void handleSignInButtonAction(Event actionEvent) {
-        signInButton.setDisable(true);
-        signUpButton.setDisable(true);
+        disableUI();
         String email = emailField.getText();
         String password = passwordField.getText();
         ServerManager.signIn(email, password, this);
@@ -61,22 +70,35 @@ public class SignInController implements SignInCallback, Initializable {
 
     @Override
     public void singInFailed(String message) {
-        this.setInfoMessage(true, message);
-        signInButton.setDisable(false);
-        signUpButton.setDisable(false);
+        enableUI(message);
     }
 
     @Override
     public void errorReceived(String errorMessage) {
-        this.setInfoMessage(true, errorMessage);
-        signInButton.setDisable(false);
-        signUpButton.setDisable(false);
+        enableUI(errorMessage);
     }
 
     //Private methods
     private void setInfoMessage(boolean error, String message) {
         infoMessageText.setFill(error ? Color.FIREBRICK : Color.GREEN);
         infoMessageText.setText(message);
+    }
+
+    private void enableUI(String message) {
+        this.setInfoMessage(true, message);
+        activityIndicator.setVisible(false);
+        signInButton.setVisible(true);
+        signUpButton.setVisible(true);
+        emailField.setEditable(true);
+        passwordField.setEditable(true);
+    }
+
+    private void disableUI() {
+        activityIndicator.setVisible(true);
+        signInButton.setVisible(false);
+        signUpButton.setVisible(false);
+        emailField.setEditable(false);
+        passwordField.setEditable(false);
     }
 
     //Navigation
@@ -89,11 +111,6 @@ public class SignInController implements SignInCallback, Initializable {
     private void showCollectionBoardScene (Stage window) {
         String title = "Collection";
         String fxmlPath = "/sample/resources/CollectionBoardView.fxml";
-        ScreenManager.getInstance().showScene(
-                window,
-                fxmlPath,
-                title,
-                1000,
-                800);
+        ScreenManager.getInstance().showScene(window, fxmlPath, title, 1000, 800);
     }
 }
--- src/sample/controller/SignUpController.java
+++ src/sample/controller/SignUpController.java
@@ -3,11 +3,14 @@ package sample.controller;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -17,21 +20,34 @@ import sample.model.ServerManager;
 import sample.model.network.callback.SignUpCallback;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SignUpController implements SignUpCallback {
+public class SignUpController implements SignUpCallback, Initializable {
 
     @FXML private Text infoMessageText;
     @FXML private PasswordField passwordField;
     @FXML private TextField emailField;
     @FXML private Button signUpButton;
     @FXML private Button goBackButton;
+    @FXML private ProgressIndicator activityIndicator;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        emailField.addEventFilter(KeyEvent.KEY_TYPED, event -> {
+            infoMessageText.setText("");
+        });
+
+        passwordField.addEventFilter(KeyEvent.KEY_TYPED, event -> {
+            infoMessageText.setText("");
+        });
+    }
 
     public void handleSignUpButtonAction(Event event) {
         String email = emailField.getText();
         String password = passwordField.getText();
         ServerManager.signUp(email, password, this);
-        signUpButton.setDisable(true);
-        goBackButton.setDisable(true);
+        disableUI();
     }
 
     public void handleGoBackButtonAction(Event event) {
@@ -48,19 +64,33 @@ public class SignUpController implements SignUpCallback {
 
     @Override
     public void singUpFailed(String message) {
-        this.setInfoMessage(true, message);
-        signUpButton.setDisable(false);
-        goBackButton.setDisable(false);
+        enableUI(message);
     }
 
     @Override
     public void errorReceived(String errorMessage) {
-        this.setInfoMessage(true, errorMessage);
-        signUpButton.setDisable(false);
-        goBackButton.setDisable(false);
+        enableUI(errorMessage);
     }
 
     //Private methods
+
+    private void enableUI(String message) {
+        this.setInfoMessage(true, message);
+        activityIndicator.setVisible(false);
+        goBackButton.setVisible(true);
+        signUpButton.setVisible(true);
+        emailField.setEditable(true);
+        passwordField.setEditable(true);
+    }
+
+    private void disableUI() {
+        activityIndicator.setVisible(true);
+        goBackButton.setVisible(false);
+        signUpButton.setVisible(false);
+        emailField.setEditable(false);
+        passwordField.setEditable(false);
+    }
+
     private void setInfoMessage(boolean error, String message) {
         infoMessageText.setFill(error ? Color.FIREBRICK : Color.GREEN);
         infoMessageText.setText(message);
